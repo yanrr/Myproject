b47b214cbdc17aec7bf03e76c142e1586397b671
yanrr <454814890@qq.com>
2011-07-23 19:21:43 +0800

    void gcc -Wall warning

--- yanrr-project/P1/hello.c
+++ yanrr-project/P1/hello.c
@@ -1,10 +1,9 @@
 #include<stdio.h>
-int main()
+int main()  //void main()也可以执行，但不规范
 {
     int i;
     for (i=0;i<10;i++)
         {
-            /* code */
             print("helloword!\n");
         }
 
