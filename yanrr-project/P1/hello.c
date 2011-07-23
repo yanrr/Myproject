411987e6073b9664acdabd8a660e0af970d40bb5
yanrr <454814890@qq.com>
2011-07-23 19:48:04 +0800

    fuction structure
    fuction declaration
    
    function call
    
    fuction definition

--- yanrr-project/P1/hello.c
+++ yanrr-project/P1/hello.c
@@ -1,8 +1,16 @@
 #include<stdio.h>
-#define HOW_MANY_TIMES_TO_SAY_HELLO 10000
+#define HOW_MANY_TIMES_TO_SAY_HELLO 3
+void say_three_hi();
+
 int main(int argc, const char *argv[])
 {
-    int i;      //必须先定义了才能使用
+    say_three_hi();
+    return 0;
+}
+
+void say_three_hi();
+{
+    int i;      
     for (i=0;i<HOW_MANY_TIMES_TO_SAY_HELLO;i++)
         {
             print("helloword!\n");
