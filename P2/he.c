099b81bbf44614eff25ef2e666894dddb944e59e
yanrr <454814890@qq.com>
2011-07-27 18:17:53 +0800

    把一个数的二进制截取某一段，然后输出:

--- P2/he.c
+++ P2/he.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 int main(int argc, const char *argv[])
 {
-        char a,b;
-        scanf("%c",&a);
-        b = a << 5;
-        printf("%d\n",b);
+        int a,b;
+        char mask;
 
+        scanf("%d",&a);
+        mask = 0x3c;
+        b = a & mask;
+        b >>= 2;
+
+        printf("%d\n",b);
         return 0;
 }
+
