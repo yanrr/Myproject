5c042c287dc00a3359eae160bbb2ca4f5486c283
yanrr <454814890@qq.com>
2011-07-29 11:38:52 +0800

    用指针复制字符串

--- P2/str.c
+++ P2/str.c
@@ -3,22 +3,40 @@ int main(int argc, const char *argv[])
 {
         char S[] = "ABCDEFGH";
         char *P;
-        
-        P = S;     //将数组S的地址付给P的同时，也把S的值给了P
-        while (*P != '\0')
+        char Q[10];
+        P = S;
+
+        while (*P != '\0')      //在这个程序中，它是复制一个，打印一个，所以才会打印出完整的字符。但是它后来复制的内容会覆盖之前的，所以最终的Q中只是S的最后一个字符
         {
-             printf("%c",*P);
-             P++;
+            *Q = *P;
+            printf("%c",*Q);
+            P++;
         }
         printf("\n");
+        return 0;
+} 
 
-        P--;    //用指针表示字符串时，是以'\0'结尾的，P--才能回到字符串
-        while (P >= S)     //数组名的地址就是它的首地址
-        {
-            printf("%c",*P);
-            P--;
-        }
+The teacher's example：
 
-        printf("\n");
+#include <stdio.h>
+#define N 20
+char *my_copy(char *d,char *s)
+{
+    while (*s != '\0')  //char *m = d;
+    {                   //while((*(d++) = *(s++)) !='0');
+        *d = *s;        //return m;
+        d++;            //自定义函数的内容可用注释的这部分代替,且比其规范
+        s++;
+    }
+    *d = '\0';
+    return d;
+} 
+int main(int argc, const char *argv[])
+{
+        char str[N] = "hello word!";
+        char c_str[N];
+
+        my_copy(c_str,str);       //之所以调用注释部分的函数，是因为其简捷，且规范。若调用自定义函数，会出现一个严重的bug:因为它只能打印复制的最后一个字符.在这个程序中之所以正常执行，是因为数组名的地址就是数组的首地址。
+        printf("%s\n",c_str);
         return 0;
 }
