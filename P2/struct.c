30020c096a9e5512bc82e28c38e0d8926004c815
yanrr <454814890@qq.com>
2011-08-01 17:28:26 +0800

    用malloc为结构体开辟空间

--- P2/struct.c
+++ P2/struct.c
@@ -1,11 +1,11 @@
-#include <stdio.h>     // 多个结构体用指针相连构成链表，其中的结构体称为节点（node）
+#include <stdio.h>
 #include <string.h>
+#include<stdlib.h>
 
 struct complex_struct
 {
          int age;
          char name[20];
-         struct complex_struct *P1;
 };
 
 typedef struct complex_struct STU;      
@@ -13,19 +13,21 @@ typedef struct complex_struct STU;
 int main(int argc, const char *argv[])
 {
         STU m;
-        STU n;
-        STU *P2;
-        
-        P2 = &m;
-        P2->P1 = &n;
-        
-        P2->age = 11;
-        strcpy(P2->name,"yanxx");
-        P2->P1->age = 22;
-        strcpy(P2->P1->name, "yanrr");
+        STU *P1 = NULL;             //P1初始化为空
+        P1 = malloc(sizeof(STU));   //为P1开辟空间，其大小为STU的长度
 
-        printf("name:%s   age:%d\n",P2->name,P2->age);
-        printf("name:%s   age:%d\n",P2->P1->name,P2->P1->age);
+        if (P1 == NULL)
+        {
+                perror("malloc");   //若开辟空间出错，打印出其位置
+                exit(0);            //若开辟空间出错，则退出
+        }
 
+  //    P1 = &m;                    其改变了之前malloc开辟的空间，与后面的free不匹配
+        P1->age  = 22;
+        strcpy(P1->name, "yanrr");
+
+        printf("name:%s   age:%d\n",P1->name,P1->age);
+
+        free(P1);                   //释放出之前开辟的空间
         return 0;
 }
