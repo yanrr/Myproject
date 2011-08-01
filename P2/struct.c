9596c0b18eee4b3f9f937019f658b122d8cdd072
yanrr <454814890@qq.com>
2011-08-01 16:56:41 +0800

    两个节点的链表

--- P2/struct.c
+++ P2/struct.c
@@ -1,29 +1,31 @@
-#include <stdio.h>
+#include <stdio.h>     // 多个结构体用指针相连构成链表，其中的结构体称为节点（node）
 #include <string.h>
 
 struct complex_struct
 {
-  
-    int age;
-    char name[20];
+         int age;
+         char name[20];
+         struct complex_struct *P1;
 };
-   
-typedef struct complex_struct STU;     
-  
+
+typedef struct complex_struct STU;      
+
 int main(int argc, const char *argv[])
 {
-    STU m;
-    int *p1;
-    char *p2;
-  
-    p1 = &m.age
-    p2 = m.name;
-  
-  
-    *p1 = 22;
-    strcpy(p2, "yanrr");
+        STU m;
+        STU n;
+        STU *P2;
+        
+        P2 = &m;
+        P2->P1 = &n;
+        
+        P2->age = 11;
+        strcpy(P2->name,"yanxx");
+        P2->P1->age = 22;
+        strcpy(P2->P1->name, "yanrr");
 
-    printf("name:%s   age:%d\n",p2, *p1);
-    return 0;
+        printf("name:%s   age:%d\n",P2->name,P2->age);
+        printf("name:%s   age:%d\n",P2->P1->name,P2->P1->age);
+
+        return 0;
 }
-
